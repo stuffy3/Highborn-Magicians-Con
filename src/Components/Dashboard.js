0280c9638da956e75bc6d4472205109ccf540884
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2022-02-16 17:37:20 -0700

    added other components to begin building the website

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './DashboardStyles.css'
+export default function Dashboard() {
+  return (
+    <div>
+        <div className='background_Dashboard'>
+
+        </div>
+    </div>
+  )
+}
